ec0cebb224c4956ff6a56678cb4fc43f6ef08a51
Evgeny <61007171+m1nz1k@users.noreply.github.com>
2024-03-30 00:08:37 +0700

    Delete req.py
    Лишний комит (не пытайтесь юзать токен, я изменил его:) )

--- req.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def console_send(command):
-    console_response = requests.get(f'https://www.myarena.ru/api.php?query=consolecmd&cmd={command}&token=666660f80a99eeab8298568a9ef0b374')
-    print(console_response.json())
-
-console_send('meta list')
